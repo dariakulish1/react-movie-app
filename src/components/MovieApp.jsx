c343028bda43de9f9301a69401eb63fa50c4e11a
dariakulish1 <dashaklsh1507@gmail.com>
2024-02-20 17:24:47 +0200

    add fetch

--- src/components/MovieApp.jsx
+++ src/components/MovieApp.jsx
@@ -4,6 +4,12 @@ import { MovieList } from './MovieList/MovieList';
 import './style/MovieApp.scss';
 
 export const MovieApp = () => {
+  const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+  fetch('https://api.themoviedb.org/3/authentication', options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
   return (
     <div className="MovieDiv">
       <NavBar />
