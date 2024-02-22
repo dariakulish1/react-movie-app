0eeab4edd0171a7803e3301517f0fbbab7723131
dariakulish1 <dashaklsh1507@gmail.com>
2024-02-22 18:12:20 +0200

    added icons

--- src/components/FlexBoxes/FlexBoxes.jsx
+++ src/components/FlexBoxes/FlexBoxes.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './FlexBoxes.scss';
 import { ServerImg } from '../../images/ServerImg.png';
-
+import { Bounding } from '../../images/bounding-24.svg';
+import { SolidStar } from '../../images/star-solid.svg';
 export const FlexBoxes = () => {
   return (
     <div className="FlexBox">
       {/* <ServerImg /> */}
       <div className="ArtDiv">
         <div className="Rating">
+          {/* <SolidStar /> */}
           <p className="RatingNum">8.0</p>
         </div>
-        <div className="SavedMovie"></div>
+        <div className="SavedMovie">{/* <Bounding /> */}</div>
         <div className="MovieTitles">
           <h3>Movie title</h3>
         </div>
