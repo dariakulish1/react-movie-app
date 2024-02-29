c5fa19fefac29c31d97d5845712db489d16338b8
dariakulish1 <dashaklsh1507@gmail.com>
2024-02-29 14:34:06 +0200

    changed nav-div menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,15 +6,17 @@ import cinefilm from '../../images/far magnifying-glass.svg';
 export const NavBar = () => {
   return (
     <>
-      <div className="nav-div container">
-        <header className="NavTop">
-          <NavLink to="/" className="NameLabel">
-            <img src={cinefilm} />
-            STRICHKA
-          </NavLink>
-          <NavLink className="FavoriteMovie" to="/favorites">
-            Favorites
-          </NavLink>
+      <div className="nav-div">
+        <header className="nav-div__navtop">
+          <div className="nav-div__navtop__pages">
+            <NavLink to="/" className="NameLabel">
+              <img src={cinefilm} />
+              STRICHKA
+            </NavLink>
+            <NavLink className="FavoriteMovie" to="/favorites">
+              Favorites
+            </NavLink>
+          </div>
         </header>
         <Outlet />
       </div>
